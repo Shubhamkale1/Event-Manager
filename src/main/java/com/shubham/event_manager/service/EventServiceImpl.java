14ca57a435de4be1649bfa9c1809e3c6d3b208da
Shubhamkale1 <kaleshubham602@gmail.com>
2026-04-09 17:53:27 +0530

    updated — now uses DTO

--- src/main/java/com/shubham/event_manager/service/EventServiceImpl.java
+++ src/main/java/com/shubham/event_manager/service/EventServiceImpl.java
@@ -1,49 +1,62 @@
 package com.shubham.event_manager.service;
 
+import com.shubham.event_manager.dto.EventDTO;
 import com.shubham.event_manager.entity.Event;
 import com.shubham.event_manager.exception.ResourceNotFoundException;
+import com.shubham.event_manager.mapper.EventMapper;
 import com.shubham.event_manager.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class EventServiceImpl implements EventService{
     private final EventRepository eventRepository;
+    private final EventMapper eventMapper;
 
     @Override
-    public List<Event> getAllEvents() {
-        return eventRepository.findAll();
+    public List<EventDTO> getAllEvents() {
+        return eventRepository.findAll()
+                .stream()
+                .map(eventMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Event getEventById(Long id) {
-        return eventRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Event not found with id: " + id));
+    public EventDTO getEventById(Long id) {
+        Event event = eventRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException(
+                "Event not found with id: " + id));
+        return eventMapper.toDTO(event);
     }
 
     @Override
-    public Event createEvent(Event event) {
-        return eventRepository.save(event);
+    public EventDTO createEvent(EventDTO eventDTO) {
+        Event event = eventMapper.toEntity(eventDTO);
+        Event saved = eventRepository.save(event);
+        return eventMapper.toDTO(saved);
     }
 
     @Override
-    public Event updateEvent(Long id, Event updatedEvent) {
-        Event existing = getEventById(id);
-        existing.setTitle(updatedEvent.getTitle());
-        existing.setDescription(updatedEvent.getDescription());
-        existing.setEventDate(updatedEvent.getEventDate());
-        existing.setLocation(updatedEvent.getLocation());
-        existing.setCapacity(updatedEvent.getCapacity());
+    public EventDTO updateEvent(Long id, EventDTO eventDTO) {
+        Event existing = eventRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Event not found with id:" + id));
+        eventMapper.updateEventFromDTO(eventDTO,existing);
+        Event saved = eventRepository.save(existing);
+        return eventMapper.toDTO(saved);
 
-        return eventRepository.save(existing);
+
     }
 
     @Override
     public void deleteEvent(Long id) {
-        Event existing = getEventById(id);
+        Event existing = eventRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Event not found with id:" + id));
         eventRepository.delete(existing);
-
     }
 }
